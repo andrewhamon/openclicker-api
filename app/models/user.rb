6bec8727ee5939102fd3d496b1dc8f453f657450
Andrew Hamon <andrew@hamon.cc>
2016-01-16 08:54:25 -0500

    Downcase email on user and validate email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  token           :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
   has_secure_password
+
+  before_validation :generate_token
+  before_validation :downcase_email, if: :email_changed?
+
   validates :email, presence: true
   validates :email, uniqueness: true, if: :email_changed?
   validates :token, presence: true
 
-  before_validation :generate_token
-  before_validation :downcase_email
-
   private
 
   def generate_token
-    self.token = SecureRandom.uuid
+    begin
+      self.token = SecureRandom.uuid
+    end while self.class.exists?(token: token)
   end
 
   def downcase_email
